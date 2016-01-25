d51e7233cfdc1d03a8ccf6770878b781c70a3ae8
Ted Eberhard <eberhardtm@appstate.edu>
2016-01-25 13:54:16 -0500

    Add import of departments and locations to sql install file

--- boost/install.sql
+++ boost/install.sql
@@ -9,6 +9,7 @@ CREATE TABLE systems_location (
 CREATE TABLE systems_department (
        id serial,
        description character varying,
+       parent integer default NULL,
        PRIMARY KEY (id)
 );
 
@@ -110,10 +111,71 @@ CREATE TABLE systems_permission (
 CREATE TRIGGER versioning_trigger BEFORE INSERT OR UPDATE OR DELETE ON systems_device FOR EACH ROW EXECUTE PROCEDURE versioning('sys_period', 'systems_device_history', true);
 
 insert into systems_location ("description") values ('NONE');
+insert into systems_location ("description") values ('App Heights');
+insert into systems_location ("description") values ('Appalachian Hall');
+insert into systems_location ("description") values ('Appalachian Panhellenic Hall');
+insert into systems_location ("description") values ('B.B. Dougherty');
+insert into systems_location ("description") values ('Belk Hall');
+insert into systems_location ("description") values ('Bowie Hall');
+insert into systems_location ("description") values ('Camp Broadstone');
+insert into systems_location ("description") values ('Cannon Hall');
+insert into systems_location ("description") values ('Coltrane Hall');
+insert into systems_location ("description") values ('Cone Hall');
+insert into systems_location ("description") values ('Doughton Hall');
+insert into systems_location ("description") values ('East Hall');
+insert into systems_location ("description") values ('Eggers Hall');
+insert into systems_location ("description") values ('Frank Hall');
+insert into systems_location ("description") values ('Gardner Hall');
+insert into systems_location ("description") values ('Housing Warehouse');
+insert into systems_location ("description") values ('Hoey Hall');
 insert into systems_location ("description") values ('JET');
+insert into systems_location ("description") values ('Justice Hall');
+insert into systems_location ("description") values ('Legends');
+insert into systems_location ("description") values ('Living Learning Center');
+insert into systems_location ("description") values ('Lovell Hall');
+insert into systems_location ("description") values ('Mountaineer Hall');
+insert into systems_location ("description") values ('Newland Hall');
+insert into systems_location ("description") values ('State Farm');
+insert into systems_location ("description") values ('SRC');
+insert into systems_location ("description") values ('Student Support');
+insert into systems_location ("description") values ('Student Union');
+insert into systems_location ("description") values ('Summit Hall');
+insert into systems_location ("description") values ('Quin');
+insert into systems_location ("description") values ('White Hall');
+insert into systems_location ("description") values ('Winkler Hall');
+
 insert into systems_department ("description") values ('NONE');
+insert into systems_department ("description") values ('Career Development');
+insert into systems_department ("description","parent") values ('Peer Career',lastval()-1);
+insert into systems_department ("description") values ('CSIL');
+insert into systems_department ("description","parent") values ('BSA',lastval()-1);
+insert into systems_department ("description","parent") values ('Club Hub',lastval()-2);
+insert into systems_department ("description","parent") values ('Greek Office',lastval()-3);
+insert into systems_department ("description","parent") values ('Student Government',lastval()-4);
+insert into systems_department ("description","parent") values ('Student Publication',lastval()-5);
+insert into systems_department ("description") values ('Counseling Services');
+insert into systems_department ("description") values ('Child Development');
+insert into systems_department ("description") values ('Dean of Students');
+insert into systems_department ("description","parent") values ('Parent and Family Services',lastval()-1);
 insert into systems_department ("description") values ('ESS');
-insert into systems_device_type ("id","description") values (1,'pc');
+insert into systems_department ("description") values ('Health Services');
+insert into systems_department ("description") values ('Multicultural Student Development');
+insert into systems_department ("description","parent") values ('LGBT',lastval()-1);
+insert into systems_department ("description","parent") values ('Multicultural Center',lastval()-2);
+insert into systems_department ("description","parent") values ('Womans Center',lastval()-3);
+insert into systems_department ("description") values ('Student Conduct');
+insert into systems_department ("description") values ('Student Development Admin');
+insert into systems_department ("description") values ('Student Programs');
+insert into systems_department ("description","parent") values ('ACT',lastval()-1);
+insert into systems_department ("description","parent") values ('APPS',lastval()-2);
+insert into systems_department ("description","parent") values ('Legends',lastval()-3);
+insert into systems_department ("description","parent") values ('White Water',lastval()-4);
+insert into systems_department ("description") values ('University Housing');
+insert into systems_department ("description") values ('University Recreation');
+insert into systems_department ("description","parent") values ('Outdoor Programs',lastval()-1);
+insert into systems_department ("description") values ('Wellness Center');
+
+Insert into systems_device_type ("id","description") values (1,'pc');
 insert into systems_device_type ("id","description") values (2,'server');
 insert into systems_device_type ("id","description") values (3,'ipad');
 insert into systems_device_type ("id","description") values (4,'printer');
--- javascript/systems.js
+++ javascript/systems.js
@@ -11,6 +11,7 @@ $(window).load(function() {
     //systems_camera_tab.start();
     //systems_sign_tab.start();
     //systems_clock_tab.start();
+    mac.on("keyup", formatMAC);
 });
 
 function SystemsTab() {
@@ -186,3 +187,15 @@ function SystemsPrinter() {
     };
     
 }
+
+function formatMAC(e) {
+    var r = /([a-f0-9]{2})([a-f0-9]{2})/i,
+        str = e.target.value.replace(/[^a-f0-9]/ig, "");
+    
+    while (r.test(str)) {
+        str = str.replace(r, '$1' + ':' + '$2');
+    }
+
+    e.target.value = str.slice(0, 17);
+};
+
