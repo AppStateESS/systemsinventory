beb24a1c2d546f2f8be3b8c5076ec4959b5d9d44
Matthew McNaney <mcnaney@gmail.com>
2017-05-19 11:15:07 -0400

    Moved code from Controller

--- class/Factory/Search.php
+++ class/Factory/Search.php
@@ -3,6 +3,8 @@
 namespace systemsinventory\Factory;
 
 use systemsinventory\Resource\SystemDevice as Resource;
+use phpws2\Database;
+use phpws2\Database\Conditional;
 
 /**
  * @license http://opensource.org/licenses/lgpl-3.0.html
@@ -11,35 +13,139 @@ use systemsinventory\Resource\SystemDevice as Resource;
 class Search extends \phpws2\ResourceFactory
 {
 
-    public static function form(\Canopy\Request $request, $command = null)
-    {
-        javascript('jquery');
-        \phpws2\Form::requiredScript();
+    public $offset = 0;
 
-        if (empty($command))
-            $command = 'run_search';
-        $system_locations = \systemsinventory\Factory\SystemDevice::getSystemLocations();
-        $location_options = '<option value="0">All</option>';
-        foreach ($system_locations as $val) {
-            $location_options .= '<option value="' . $val['id'] . '">' . $val['display_name'] . '</option>';
+    public function getDevices($request)
+    {
+        $db = Database::getDB();
+        $sd = $db->addTable('systems_device');
+        $deptbl = $db->addTable('systems_department');
+        $loctbl = $db->addTable('systems_location');
+        $deptbl->addField('display_name', 'department_name');
+        $loctbl->addField('display_name', 'location_name');
+        $exp = new \phpws2\Database\Expression('systems_device.*, count(*) OVER() as full_count');
+        $sd->addField($exp);
+        $db->joinResources($sd, $deptbl,
+                new Conditional($db, $sd->getField('department_id'),
+                $deptbl->getField('id'), '='));
+        $db->joinResources($sd, $loctbl,
+                new Conditional($db, $sd->getField('location_id'),
+                $loctbl->getField('id'), '='));
+        $conditional = $this->createSearchConditional($db, $sd, $request);
+        if (!empty($conditional)) {
+            $db->addConditional($conditional);
         }
-        $vars['locations'] = $location_options;
-        $system_types = \systemsinventory\Factory\SystemDevice::getSystemTypes();
-        $type_options = '<option value="0">All</option>';
-        foreach ($system_types as $val) {
-            $type_options .= '<option value="' . $val['id'] . '">' . $val['description'] . '</option>';
+
+        // -1 means show everything at once
+        if ($this->offset !== -1) {
+            $db->setLimit(SYSINV_ROWS, SYSINV_ROWS * $this->offset);
         }
-        $vars['system_types'] = $type_options;
+
+        $this->sort($request, $sd, $deptbl, $loctbl);
+        $result = $db->select();
+        return $result;
+    }
+
+    private function createSearchConditional(\phpws2\Database\DB $db,
+            \phpws2\Database\Table $tbl, $request)
+    {
+        $conditional = NULL;
         $system_dep = \systemsinventory\Factory\SystemDevice::getSystemDepartments();
-        $dep_optons = '<option value="0">All</option>';
-        foreach ($system_dep as $val) {
-            $dep_optons .= '<option value="' . $val['id'] . '">' . $val['display_name'] . '</option>';
+
+        $system_type = $request->pullGetArray('systemType', true);
+        $department_id = $request->pullGetString('department', true);
+        $location_id = $request->pullGetString('location', true);
+        $physical_id = $request->pullGetString('physicalId', true);
+        $model = $request->pullGetString('model', true);
+        $username = $request->pullGetString('username', true);
+        $purchase_date = $request->pullGetString('purchaseDate', true);
+        $ip = $request->pullGetString('ipAddress', true);
+        $mac = $request->pullGetString('macAddress', true);
+
+        if ($system_type[0] !== 'all') {
+            $tbl->addFieldConditional('device_type_id', $system_type, 'in');
         }
-        $vars['departments'] = $dep_optons;
-        $vars['form_action'] = "./systemsinventory/search/" . $command;
-        $template = new \phpws2\Template($vars);
-        $template->setModuleTemplate('systemsinventory', 'Search_System.html');
-        return $template->get();
+
+        foreach ($system_dep as $val) {
+            $permitted_ids[] = $val['id'];
+        }
+
+        if ($department_id && in_array($department_id, $permitted_ids)) {
+            $tbl->addFieldConditional('department_id', $department_id);
+        } else {
+            $tbl->addFieldConditional('department_id', $permitted_ids, 'in');
+        }
+
+        if ($location_id) {
+            $tbl->addFieldConditional('location_id', $location_id);
+        }
+
+        if (!empty($physical_id)) {
+            $tbl->addFieldConditional('physical_id', "%$physical_id%", 'like');
+        }
+
+        if (!empty($model)) {
+            $tbl->addFieldConditional('model', "%$model%", 'like');
+        }
+
+        if (!empty($username)) {
+            $tbl->addFieldConditional('username', "%$username%", 'LIKE');
+        }
+
+        if (!empty($purchase_date)) {
+            $from_date = strtotime($purchase_date);
+            $to_date = strtotime($purchase_date) + 86400;
+            $tbl->addFieldConditional('purchase_date', $from_date, '>');
+            $tbl->addFieldConditional('purchase_date', $to_date, '<');
+        }
+
+        if (!empty($ip)) {
+            $c1 = $tbl->getFieldConditional('primary_ip', "%$ip%", 'like');
+            $c2 = $tbl->getFieldConditional('secondary_ip', "%$ip%", 'like');
+            $db->addConditional($c1, $c2, 'or');
+        }
+
+        if (!empty($search_vars['mac'])) {
+            $c1 = $tbl->getFieldConditional('mac', "%$mac%", 'like');
+            $c2 = $tbl->getFieldConditional('mac2', "%$mac%", 'like');
+            $db->addConditional($c1, $c2, 'or');
+        }
+        return $conditional;
+    }
+
+    private function sort($request, \phpws2\Database\Table $system_table,
+            \phpws2\Database\Table $dept_table,
+            \phpws2\Database\Table $location_table)
+    {
+        $sort = $request->pullGetArray('sort', true);
+        if ($sort && $sort['direction'] !== '0') {
+            $direction = $sort['direction'] === '1' ? 'asc' : 'desc';
+            switch ($sort['column']) {
+                case 'physical':
+                    $system_table->addOrderBy('physical_id', $direction);
+                    break;
+
+                case 'model':
+                    $system_table->addOrderBy('model', $direction);
+                    break;
+
+                case 'location':
+                    $location_table->addOrderBy('display_name', $direction);
+                    break;
+
+                case 'room':
+                    $system_table->addOrderBy('room_number', $direction);
+                    break;
+
+                case 'username':
+                    $system_table->addOrderBy('username', $direction);
+                    break;
+
+                case 'department':
+                    $dept_table->addOrderBy('display_name', $direction);
+                    break;
+            }
+        }
     }
 
 }
