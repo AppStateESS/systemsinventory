116cb9761b3a156c550d7adf48567f7e39815c0a
Matthew McNaney <mcnaney@gmail.com>
2017-05-19 11:14:05 -0400

    Moved code to Search factory class. Added report download functionality

--- class/Controller/Search.php
+++ class/Controller/Search.php
@@ -33,10 +33,54 @@ class Search extends \phpws2\Http\Controller
     protected function getHtmlView($data, \Canopy\Request $request)
     {
         \Layout::addStyle('systemsinventory');
-//$content = Factory::form($request);
-        $content = $this->getFilterScript() . React::view('search');
+        $command = $request->shiftCommand();
+        switch ($command) {
+            case 'download':
+                $content = $this->download($request);
+                break;
+
+            default:
+                $content = $this->view();
+        }
         $view = new \phpws2\View\HtmlView($content);
         return $view;
+    }
+
+    private function view()
+    {
+        $content = $this->getFilterScript() . React::view('search');
+        return $content;
+    }
+
+    private function download($request)
+    {
+        $system_type = $request->pullGetArray('systemType', true);
+        $file_name =  \Canopy\TextString::randomString(12) . '.csv';
+        $file_location = "/tmp/$file_name";
+
+        if (empty($system_type)) {
+            $result = null;
+        } else {
+            $factory = new Factory;
+            $factory->offset = -1;
+            $result = $factory->getDevices($request);
+            if (empty($result)) {
+                return 'No results found';
+            }
+            $fp = fopen($file_location, 'w');
+
+            fputcsv($fp, array_keys($result[0]));
+
+            foreach ($result as $row) {
+                fputcsv($fp, $row);
+            }
+            fclose($fp);
+        }
+        header("Content-Type: application/octet-stream");
+        header("Content-Transfer-Encoding: Binary");
+        header("Content-disposition: attachment; filename=\"$file_name\"");
+        echo readfile($file_location);
+        exit();
     }
 
     protected function getFilterScript()
@@ -74,79 +118,21 @@ EOF;
         return $filters;
     }
 
-    private function sort($request, \phpws2\Database\Table $system_table,
-            \phpws2\Database\Table $dept_table,
-            \phpws2\Database\Table $location_table)
-    {
-        $sort = $request->pullGetArray('sort', true);
-        if ($sort && $sort['direction'] !== '0') {
-            $direction = $sort['direction'] === '1' ? 'asc' : 'desc';
-            switch ($sort['column']) {
-                case 'physical':
-                    $system_table->addOrderBy('physical_id', $direction);
-                    break;
-
-                case 'model':
-                    $system_table->addOrderBy('model', $direction);
-                    break;
-
-                case 'location':
-                    $location_table->addOrderBy('display_name', $direction);
-                    break;
-
-                case 'room':
-                    $system_table->addOrderBy('room_number', $direction);
-                    break;
-
-                case 'username':
-                    $system_table->addOrderBy('username', $direction);
-                    break;
-
-                case 'department':
-                    $dept_table->addOrderBy('display_name', $direction);
-                    break;
-            }
-        }
-    }
-
     protected function getJsonView($data, \Canopy\Request $request)
     {
         $total = 0;
         $total_shown = 0;
         $more = false;
         $system_type = $request->pullGetArray('systemType', true);
-        $offset = 0;
 
         if (empty($system_type)) {
             $result = null;
         } else {
-            $db = Database::getDB();
-            $sd = $db->addTable('systems_device');
-            $deptbl = $db->addTable('systems_department');
-            $loctbl = $db->addTable('systems_location');
-            $deptbl->addField('display_name', 'department_name');
-            $loctbl->addField('display_name', 'location_name');
-            $exp = new \phpws2\Database\Expression('systems_device.*, count(*) OVER() as full_count');
-            $sd->addField($exp);
-            $db->joinResources($sd, $deptbl,
-                    new Conditional($db, $sd->getField('department_id'),
-                    $deptbl->getField('id'), '='));
-            $db->joinResources($sd, $loctbl,
-                    new Conditional($db, $sd->getField('location_id'),
-                    $loctbl->getField('id'), '='));
-            $conditional = $this->createSearchConditional($db, $sd, $request);
-            if (!empty($conditional)) {
-                $db->addConditional($conditional);
-            }
-
+            $factory = new Factory;
             $offsetMult = $request->pullGetInteger('offset', true);
-            // -1 means show everything at once
-            if ($offsetMult !== -1) {
-                $db->setLimit(SYSINV_ROWS, SYSINV_ROWS * $offsetMult);
-            }
+            $factory->offset = $offsetMult;
+            $result = $factory->getDevices($request);
 
-            $this->sort($request, $sd, $deptbl, $loctbl);
-            $result = $db->select();
             if ($result) {
                 // Total number of rows not adjusted for limit
                 if (isset($result[1])) {
@@ -171,87 +157,6 @@ EOF;
 
         return parent::getJsonView(array('listing' => $result, 'total' => $total, 'shown' => $total_shown, 'more' => $more),
                         $request);
-    }
-
-    private function createSearchConditional(\phpws2\Database\DB $db,
-            \phpws2\Database\Table $tbl, $request)
-    {
-        $conditional = NULL;
-        $system_dep = \systemsinventory\Factory\SystemDevice::getSystemDepartments();
-
-        $system_type = $request->pullGetArray('systemType', true);
-        $department_id = $request->pullGetString('department', true);
-        $location_id = $request->pullGetString('location', true);
-        $physical_id = $request->pullGetString('physicalId', true);
-        $model = $request->pullGetString('model', true);
-        $username = $request->pullGetString('username', true);
-        $purchase_date = $request->pullGetString('purchaseDate', true);
-        $ip = $request->pullGetString('ipAddress', true);
-        $mac = $request->pullGetString('macAddress', true);
-
-        if ($system_type[0] !== 'all') {
-            $tbl->addFieldConditional('device_type_id', $system_type, 'in');
-        }
-
-        foreach ($system_dep as $val) {
-            $permitted_ids[] = $val['id'];
-        }
-
-        if ($department_id && in_array($department_id, $permitted_ids)) {
-            $tbl->addFieldConditional('department_id', $department_id);
-        } else {
-            $tbl->addFieldConditional('department_id', $permitted_ids, 'in');
-        }
-
-        if ($location_id) {
-            $tbl->addFieldConditional('location_id', $location_id);
-        }
-
-        if (!empty($physical_id)) {
-            $tbl->addFieldConditional('physical_id', "%$physical_id%", 'like');
-        }
-
-        if (!empty($model)) {
-            $tbl->addFieldConditional('model', "%$model%", 'like');
-        }
-
-        if (!empty($username)) {
-            $tbl->addFieldConditional('username', "%$username%", 'LIKE');
-        }
-
-        if (!empty($purchase_date)) {
-            $from_date = strtotime($purchase_date);
-            $to_date = strtotime($purchase_date) + 86400;
-            $tbl->addFieldConditional('purchase_date', $from_date, '>');
-            $tbl->addFieldConditional('purchase_date', $to_date, '<');
-        }
-
-        if (!empty($ip)) {
-            $c1 = $tbl->getFieldConditional('primary_ip', "%$ip%", 'like');
-            $c2 = $tbl->getFieldConditional('secondary_ip', "%$ip%", 'like');
-            $db->addConditional($c1, $c2, 'or');
-        }
-
-        if (!empty($search_vars['mac'])) {
-            $c1 = $tbl->getFieldConditional('mac', "%$mac%", 'like');
-            $c2 = $tbl->getFieldConditional('mac2', "%$mac%", 'like');
-            $db->addConditional($c1, $c2, 'or');
-        }
-        return $conditional;
-    }
-
-    /**
-     * Format the search row by translating id's to names and formatting the date to human readable
-     *
-     * @param array $row
-     * @return array
-     */
-    public static function alterSearchRow($row)
-    {
-        $row['department_id'] = \systemsinventory\Factory\SystemDevice::getDepartmentByID($row['department_id']);
-        $row['location_id'] = \systemsinventory\Factory\SystemDevice::getLocationByID($row['location_id']);
-        $row['purchase_date'] = date('n/d/Y', $row['purchase_date']);
-        return $row;
     }
 
     /**
