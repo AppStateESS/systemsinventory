5fae1d59349a58509d2bd01700136048d54c393e
Matthew McNaney <mcnaney@gmail.com>
2017-05-19 11:15:39 -0400

    Location and department seaching working

--- javascript/Search/Listing.jsx
+++ javascript/Search/Listing.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import DeviceRow from './DeviceRow.jsx'
 import Sort from './Sort.jsx'
 import TooltipSearch from './TooltipSearch.jsx'
+import TooltipSelectSearch from './TooltipSelectSearch.jsx'
 
 export default class Listing extends Component {
   constructor(props) {
@@ -47,24 +48,28 @@ export default class Listing extends Component {
   }
 
   search() {
-    const {filters} = this.props
+    const {filters, update, updateSelect} = this.props
     let search = {}
     search.physicalSearch = <TooltipSearch
-      value={this.props.filters.physicalId}
-      change={this.props.update.bind(null, 'physicalId')}/>
-    search.modelSearch = <TooltipSearch
-      value={this.props.filters.model}
-      change={this.props.update.bind(null, 'model')}/>
-    search.locationSearch = <i className="fa fa-search"></i>
-    search.usernameSearch = <TooltipSearch
-      value={this.props.filters.username}
-      change={this.props.update.bind(null, 'username')}/>
-    search.departmentSearch = <i className="fa fa-search"></i>
+      value={filters.physicalId}
+      change={update.bind(null, 'physicalId')}/>
+    search.modelSearch = <TooltipSearch value={filters.model} change={update.bind(null, 'model')}/>
+    search.locationSearch = <TooltipSelectSearch
+      value={filters.location}
+      options={this.props.options.locations}
+      change={updateSelect.bind(null, 'location')}/>
+    search.usernameSearch = <TooltipSearch value={filters.username} change={update.bind(null, 'username')}/>
+    search.departmentSearch = <TooltipSelectSearch
+      value={filters.department}
+      options={this.props.options.departments}
+      change={updateSelect.bind(null, 'department')}/>
     return search
   }
 
-  updateTest(e) {
-    this.setState({test: e.target.value})
+  highlight(test) {
+    return test
+      ? 'bg-success'
+      : null
   }
 
   getTable() {
@@ -82,7 +87,7 @@ export default class Listing extends Component {
     } = this.sort()
     const {physicalSearch, modelSearch, locationSearch, usernameSearch, departmentSearch} = this.search()
 
-    if (this.props.rows === null) {
+    if (this.props.rows === null || this.props.rows.length === 0) {
       return <div></div>
     } else {
       rows = this.props.rows.map(function (val, key) {
@@ -97,25 +102,16 @@ export default class Listing extends Component {
             <tbody>
               <tr className="search-header">
                 <th>Type</th>
-                <th
-                  className={filters.physicalId.length > 0
-                  ? 'bg-success'
-                  : null}>Physical ID&nbsp;&nbsp;<span>{physicalSort}&nbsp;{physicalSearch}</span>
+                <th className={this.highlight(filters.physicalId.length > 0)}>Physical ID&nbsp;&nbsp;<span>{physicalSort}&nbsp;{physicalSearch}</span>
                 </th>
-                <th
-                  className={filters.model.length > 0
-                  ? 'bg-success'
-                  : null}>Model&nbsp;&nbsp;<span>{modelSort}&nbsp;{modelSearch}</span>
+                <th className={this.highlight(filters.model.length > 0)}>Model&nbsp;&nbsp;<span>{modelSort}&nbsp;{modelSearch}</span>
                 </th>
-                <th>Location&nbsp;&nbsp;<span>{locationSort}&nbsp;{locationSearch}</span>
+                <th className={this.highlight(filters.location > 0)}>Location&nbsp;&nbsp;<span>{locationSort}&nbsp;{locationSearch}</span>
                 </th>
                 <th>Room&nbsp;&nbsp;{roomSort}</th>
-                <th
-                  className={filters.username.length > 0
-                  ? 'bg-success'
-                  : null}>Username&nbsp;&nbsp;<span>{usernameSort}&nbsp;{usernameSearch}</span>
+                <th className={this.highlight(filters.username.length > 0)}>Username&nbsp;&nbsp;<span>{usernameSort}&nbsp;{usernameSearch}</span>
                 </th>
-                <th>Department&nbsp;&nbsp;<span>{departmentSort}&nbsp;{departmentSearch}</span>
+                <th className={this.highlight(filters.department > 0)}>Department&nbsp;&nbsp;<span>{departmentSort}&nbsp;{departmentSearch}</span>
                 </th>
               </tr>
               {rows}
@@ -142,5 +138,7 @@ Listing.propTypes = {
   filters: PropTypes.object,
   toggleSort: PropTypes.func,
   sort: PropTypes.object,
-  update: PropTypes.func
+  update: PropTypes.func,
+  updateSelect: PropTypes.func,
+  options: PropTypes.object
 }
