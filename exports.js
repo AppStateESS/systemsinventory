8889d72f25f80dedb86aeba9ae279df52920bf1a
Matthew McNaney <mcnaney@gmail.com>
2017-05-30 09:20:32 -0400

    Added "add" for new devices

--- exports.js
+++ exports.js
@@ -3,5 +3,6 @@ exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 
 exports.entry = {
   search: exports.APP_DIR + '/Search/index.jsx',
+  add: exports.APP_DIR + '/Add/index.jsx',
   vendor: ['react', 'react-dom']
 }
