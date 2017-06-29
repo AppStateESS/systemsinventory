ed6674d41644761c2505ddd9d86a8f3cd461097b
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 11:55:06 -0400

    Printer assign form finished

--- javascript/AssignForms/Printer.jsx
+++ javascript/AssignForms/Printer.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Base from '../DeviceForms/Base.jsx'
+import BigCheckbox from '../FormMixin/BigCheckbox.jsx'
 
 export default class Printer extends Base {
   constructor(props) {
@@ -10,8 +11,31 @@ export default class Printer extends Base {
   }
 
   render() {
+    const {device} = this.props
+    let placeholder
+    if (!device.network) {
+      placeholder = 'Printer is not networked'
+    }
     return (
-      <div></div>
+      <div>
+        <div className="row">
+          <div className="col-md-3 col-sm-4">
+            {this.inputField('first_name')}
+          </div>
+          <div className="col-md-3 col-sm-4">
+            {this.inputField('last_name')}
+          </div>
+          <div className="col-md-3 col-sm-4">
+            {this.inputField('username')}
+          </div>
+          <div className="col-md-3 col-sm-4">
+            {this.inputField('phone')}
+          </div>
+          <div className="col-md-3 col-sm-4">
+            {this.inputField('primary_ip', 'Primary IP', !device.network, placeholder)}
+          </div>
+        </div>
+      </div>
     )
   }
 }
