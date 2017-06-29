343747970bcaff618287e725f41eded4e737a280
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 11:42:55 -0400

    Finished assign.

--- class/Factory/PC.php
+++ class/Factory/PC.php
@@ -9,30 +9,24 @@ class PC extends SystemDevice
 
     public function postNewPC(\Canopy\Request $request, Resource $pc)
     {
-        $pc->setPrimaryMonitor(filter_input(INPUT_POST, 'primary_monitor',
-                        FILTER_SANITIZE_STRING));
-        $pc->setSecondaryMonitor(filter_input(INPUT_POST, 'secondary_monitor',
-                        FILTER_SANITIZE_STRING));
+        $pc->setPrimaryMonitor($request->pullPostString('primary_monitor'));
         $pc->setVideoCard(filter_input(INPUT_POST, 'video_card',
                         FILTER_SANITIZE_STRING));
         $pc->setOS(filter_input(INPUT_POST, 'os', FILTER_SANITIZE_STRING));
 
         $pc->setServerType(filter_input(INPUT_POST, 'server_type',
                         FILTER_SANITIZE_STRING));
-        $pc->setBatteryBackup($request->pullPostBoolean('battery_backup'));
-        $pc->setRedundantBackup($request->pullPostBoolean('redundant_backup'));
-        $pc->setRotation($request->pullPostBoolean('rotation'));
-        $pc->setStand($request->pullPostBoolean('stand'));
-        $pc->setTouchScreen($request->pullPostBoolean('touch_screen'));
-        $pc->setDual($request->pullPostBoolean('dual_monitor'));
-        $pc->setSmartRoom($request->pullPostBoolean('smart_room'));
-        $pc->setCheckIn($request->pullPostBoolean('check_in'));
-        $pc->setIsServer($request->pullPostBoolean('is_server'));
+        $pc->setBatteryBackup((bool)$request->pullPostBoolean('battery_backup', true));
+        $pc->setRedundantBackup((bool)$request->pullPostBoolean('redundant_backup', true));
+        $pc->setRotation((bool)$request->pullPostBoolean('rotation', true));
+        $pc->setStand((bool)$request->pullPostBoolean('stand', true));
+        $pc->setTouchScreen((bool)$request->pullPostBoolean('touch_screen', true));
+        $pc->setIsServer((bool)$request->pullPostBoolean('is_server', true));
 
         self::saveResource($pc);
     }
 
-    public static function assignDevice(Resource $device, $request)
+    public static function assignDevice(Resource $sdevice, $device, $request)
     {
         // if the status is 2, device is assigned to a LOCATION and we can 
         // forgive missing names
@@ -40,12 +34,16 @@ class PC extends SystemDevice
         $device->setLastName($request->pullPatchString('last_name', $device->getStatus() == 2));
         $device->setUsername($request->pullPatchString('username', $device->getStatus() == 2));
         $device->setPhone($request->pullPatchString('phone', $device->getStatus() == 2));
-        $device->setSysPeriod($request->pullPatchString('system_usage'));
         $device->setVlan($request->pullPatchInteger('vlan', true));
         if ($request->patchVarIsset('secondary_ip')) {
             $device->setSecondaryIP($request->pullPatchString('secondary_ip',
                             true));
         }
+        $sdevice->setSystemUsage($request->pullPatchInteger('system_usage'));
+        $sdevice->setSmartRoom((bool)$request->pullPatchBoolean('smart_room', true));
+        $sdevice->setCheckIn((bool)$request->pullPatchBoolean('check_in', true));
+        $sdevice->setDual((bool)$request->pullPatchBoolean('dual_monitor', true));
+        $sdevice->setSecondaryMonitor($request->pullPatchString('secondary_monitor', true));
     }
 
 }
