f84c102d090c86dcd91355092a289ea9b6425b11
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 11:40:47 -0400

    Apple Id moved to assign. Fixed case set. Fixed system usage

--- class/Factory/IPAD.php
+++ class/Factory/IPAD.php
@@ -10,20 +10,19 @@ class IPAD extends SystemDevice
     public function postNewIPAD(\Canopy\Request $request, $ipad)
     {
         $ipad->setGeneration($request->pullPostString('generation'));
-        $ipad->setAppleID($request->pullPostString('apple_id'));
-        $ipad->setProtectiveCase($request->pullPostBoolean('protective_case'));
+        $ipad->setProtectiveCase((bool)$request->pullPostBoolean('protective_case', true));
 
         self::saveResource($ipad);
     }
 
-    public static function assignDevice(Resource $device, $request)
+    public static function assignDevice(Resource $sdevice, $device, $request)
     {
         $device->setFirstName($request->pullPatchString('first_name'));
         $device->setLastName($request->pullPatchString('last_name'));
         $device->setUsername($request->pullPatchString('username'));
         $device->setPhone($request->pullPatchString('phone'));
-        $device->setSysPeriod($request->pullPatchString('system_usage'));
-        $device->setAppleID($request->pullPatchString('apple_id'));
+        $sdevice->setSystemUsage($request->pullPatchString('system_usage'));
+        $sdevice->setAppleID($request->pullPatchString('apple_id'));
     }
 
 }
