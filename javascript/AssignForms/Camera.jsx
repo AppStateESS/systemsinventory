d1831357598d7aa394132168785dc58ff655b495
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 11:51:18 -0400

    Finished camera assign

--- javascript/AssignForms/Camera.jsx
+++ javascript/AssignForms/Camera.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Base from '../DeviceForms/Base.jsx'
+import BigCheckbox from '../FormMixin/BigCheckbox.jsx'
 
 export default class Camera extends Base {
   constructor(props) {
@@ -10,8 +11,41 @@ export default class Camera extends Base {
   }
 
   render() {
+    const {device, update} = this.props
     return (
-      <div></div>
+      <div>
+        <div className="row">
+          <div className="col-sm-3">
+            {this.inputField('primary_ip', 'Primary IP')}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-3">
+            <BigCheckbox
+              checked={device.exterior === 1}
+              handle={update.bind(null, 'exterior', device.exterior === 1
+              ? 0
+              : 1)}
+              label="Exterior"/>
+          </div>
+          <div className="col-sm-3">
+            <BigCheckbox
+              checked={device.covert === 1}
+              handle={update.bind(null, 'covert', device.covert === 1
+              ? 0
+              : 1)}
+              label="Covert"/>
+          </div>
+          <div className="col-sm-3">
+            <BigCheckbox
+              checked={device.is_on === 1}
+              handle={update.bind(null, 'is_on', device.is_on === 1
+              ? 0
+              : 1)}
+              label="Activated"/>
+          </div>
+        </div>
+      </div>
     )
   }
 }
