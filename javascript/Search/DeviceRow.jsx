7ef0c59a3b984efb0bc522a062d998afc43db3e5
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 12:09:52 -0400

    Added surplus and edit assignment links

--- javascript/Search/DeviceRow.jsx
+++ javascript/Search/DeviceRow.jsx
@@ -1,6 +1,7 @@
 'use strict'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
 export default class DeviceRow extends Component {
   constructor(props) {
@@ -62,16 +63,22 @@ export default class DeviceRow extends Component {
       department_name,
       location_name,
       device_type_id,
+      purchase_date,
       status
     } = this.props.value
 
     let assign
+    let editAssign
+    let surplus
     if (status === 0) {
+      surplus = <li><a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'surplus')} className="pointer"><i className="fa fa-truck"></i>&nbsp;Surplus</a></li>
       assign = <li><a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'assign')} className="pointer"><i className="fa fa-user"></i>&nbsp;Assign</a></li>
     } else if (status === 1 || status === 2) {
+      editAssign = <li><a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'assign')} className="pointer"><i className="fa fa-pencil"></i>&nbsp;Edit assignment</a></li>
       assign = <li><a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'unassign')} className="pointer"><i className="fa fa-recycle"></i>&nbsp;Unassign</a></li>
     }
 
+    const red = {color: 'red'}
     const editButton = (
       <div className="dropdown">
         <i
@@ -83,102 +90,58 @@ export default class DeviceRow extends Component {
             <a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'view')} className="pointer"><i className="fa fa-eye"></i>&nbsp;View</a>
           </li>
           <li>
-            <a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'edit')} className="pointer"><i className="fa fa-edit"></i>&nbsp;Edit</a>
+            <a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'edit')} className="pointer"><i className="fa fa-cogs"></i>&nbsp;Edit device</a>
           </li>
+          {editAssign}
           {assign}
+          {surplus}
           <li role="separator" className="divider"></li>
           <li>
-            <a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'delete')} style={{color: 'red'}}className="pointer"><i className="fa fa-trash-o"></i>&nbsp;Delete</a>
+            <a onClick={this.props.showOverlay.bind(null, this.props.value.id, 'delete')} style={red} className="pointer"><i className="fa fa-trash-o"></i>&nbsp;Delete</a>
           </li>
         </ul>
       </div>
     )
 
+    let row = []
+    row.push(<span>{editButton}</span>)
+    row.push(<span className="text-center">{this.deviceType(device_type_id)}</span>)
+    row.push(<span>{physical_id}</span>)
+    row.push(<span>{moment.unix(purchase_date).format('YYYY-MM-DD')}</span>)
+
     switch (this.props.statusType) {
       case 0:
-        return (
-          <tr>
-            <td>{editButton}
-            </td>
-            <td className="text-center">
-              {this.deviceType(device_type_id)}
-            </td>
-            <td>
-              {physical_id}
-            </td>
-            <td>
-              {model}
-            </td>
-            <td>
-              {this.verboseStatus(status)}
-            </td>
-          </tr>
-        )
+        row.push(<span>{model}</span>)
+        row.push(<span>{this.verboseStatus(status)}</span>)
+        break
+
       case 1:
-        return (
-          <tr>
-            <td>
-              {editButton}
-            </td>
-            <td>
-              {this.deviceType(device_type_id)}
-            </td>
-            <td>
-              {physical_id}
-            </td>
-            <td>
-              {model}
-            </td>
-          </tr>
-        )
+        row.push(<span>{model}</span>)
+        break
+
       case 2:
-        return (
-          <tr>
-            <td>{editButton}
-            </td>
-            <td>
-              {this.deviceType(device_type_id)}
-            </td>
-            <td>
-              {physical_id}
-            </td>
-            <td>
-              {model}
-            </td>
-            <td>
-              {username}
-            </td>
-            <td>
-              {location_name}
-            </td>
-            <td>
-              {department_name}
-            </td>
-          </tr>
-        )
+        row.push(<span>{model}</span>)
+        row.push(<span>{username}</span>)
+        row.push(<span>{location_name}</span>)
+        row.push(<span>{department_name}</span>)
+        break
+
       case 3:
-        return (
-          <tr>
-            <td>{editButton}
-            </td>
-            <td>
-              {this.deviceType(device_type_id)}
-            </td>
-            <td>
-              {physical_id}
-            </td>
-            <td>
-              {model}
-            </td>
-          </tr>
-        )
+        row.push(<span>{model}</span>)
+        break
     }
+    return (
+      <tr>
+        {row.map(function(value,key){
+          return <td key={key}>{value}</td>
+        })}
+      </tr>
+    )
   }
 }
 
 DeviceRow.propTypes = {
   value: PropTypes.object,
-  showOverlay: PropTypes.func,
-  statusType: PropTypes.number,
-  assign: PropTypes.func
+  showOverlay: PropTypes.func.isRequired,
+  statusType: PropTypes.number.isRequired,
 }
