db3a1a91281137d98aa70d3a4ab555be3757b5cb
Matthew McNaney <mcnaney@gmail.com>
2017-11-21 13:47:05 -0500

    Copied stories buttongroup. Stateless function.

--- javascript/FormMixin/ButtonGroup.jsx
+++ javascript/FormMixin/ButtonGroup.jsx
@@ -1,73 +1,79 @@
 'use strict'
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-class ButtonGroup extends Component {
-  constructor(props) {
-    super(props)
-  }
+/**
+* Example buttons prop
+* const buttons = [{label: 'One', value: 1}, {...}]
+*
+*/
 
-  render() {
-    let buttons = this.props.buttons.map(function (value, key) {
-      const activeColor = 'btn-' + this.props.activeColor
-      let cn = classnames('btn', 'btn-default')
-      if (this.props.match !== null && this.props.match !== undefined) {
-        if (this.props.match.constructor === Array && (this.props.match.indexOf(value.value) !== -1)) {
-          cn = classnames('btn', 'active', activeColor)
-        } else if (this.props.match == value.value) {
-          cn = classnames('btn', 'active', activeColor)
-        }
+const ButtonGroup = ({
+  match,
+  buttons,
+  handle,
+  vertical,
+  name,
+  activeColor
+}) => {
+  let buttonList = buttons.map(function (value, key) {
+    const buttonColor = 'btn-' + activeColor
+    let cn = classnames('btn', 'btn-default')
+    if (match !== null && match !== undefined) {
+      if (match.constructor === Array && (match.indexOf(value.value) !== -1)) {
+        cn = classnames('btn', 'active', buttonColor)
+      } else if (match == value.value) {
+        cn = classnames('btn', 'active', buttonColor)
       }
-
-      return (
-        <button
-          type="button"
-          key={key}
-          className={cn}
-          value={value.value}
-          onClick={this.props.handle.bind(null, value.value)}>
-          {value.label}
-        </button>
-      )
-    }.bind(this))
-
-    const buttonClass = this.props.vertical === true
-      ? 'btn-group-vertical'
-      : 'btn-group'
-
-    let hidden
-    if (this.props.match && this.props.match.constructor === Array) {
-      hidden = this.props.match.map(function (value, key) {
-        let name = this.props.name + '[]'
-        return <input type="hidden" name={name} value={value} key={key}/>
-      }.bind(this))
-    } else {
-      hidden = <input type="hidden" name={this.props.name} value={this.props.match}/>
     }
-
     return (
-      <div className={buttonClass} role="group">
-        {buttons}
-        {hidden}
-      </div>
+      <button
+        type="button"
+        key={key}
+        className={cn}
+        value={value.value}
+        onClick={handle.bind(null, value.value)}>
+        {value.label}
+      </button>
     )
+  }.bind(this))
+
+  const buttonClass = vertical === true
+    ? 'btn-group-vertical'
+    : 'btn-group'
+
+  let hidden
+  if (match && match.constructor === Array) {
+    hidden = match.map(function (value, key) {
+      let name = name + '[]'
+      return <input type="hidden" name={name} value={value} key={key}/>
+    }.bind(this))
+  } else {
+    hidden = <input type="hidden" name={name} value={match}/>
   }
+
+  return (
+    <div className={buttonClass} role="group">
+      {buttonList}
+      {hidden}
+    </div>
+  )
 }
 
 ButtonGroup.propTypes = {
   buttons: PropTypes.array.isRequired,
   handle: PropTypes.func.isRequired,
   activeColor: PropTypes.string,
-  match: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
+  match: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array,]),
   vertical: PropTypes.bool,
   name: PropTypes.string
 }
 
-ButtonGroup.defaultProp = {
+ButtonGroup.defaultProps = {
   activeColor: 'default',
-  match: null,
-  name: null
+  match: '',
+  name: ''
 }
 
 export default ButtonGroup
