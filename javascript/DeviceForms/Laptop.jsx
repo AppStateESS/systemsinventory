920a012af1646e3c27fc1d01d9031f1af0fae487
Ted <eberhardtm@appstate.edu>
2018-10-19 10:13:09 -0400

    Some fixes for the laptop add

--- javascript/DeviceForms/Laptop.jsx
+++ javascript/DeviceForms/Laptop.jsx
@@ -1,117 +1,119 @@
 'use strict'
 import React from 'react'
-import Base from './Base.jsx'
-import InputField from '../FormMixin/InputField.jsx'
-import BigCheckbox from '../FormMixin/BigCheckbox.jsx'
-import SelectFilter from '../FormMixin/SelectFilter.jsx'
-import empty from '../Mixin/Empty.js'
+        import Base from './Base.jsx'
+        import InputField from '../FormMixin/InputField.jsx'
+        import BigCheckbox from '../FormMixin/BigCheckbox.jsx'
+        import SelectFilter from '../FormMixin/SelectFilter.jsx'
+        import empty from '../Mixin/Empty.js'
 
-export default class Laptop extends Base {
-  constructor(props) {
-    super(props)
-    this.deviceType = 'laptop'
-  }
+        export default class Laptop extends Base {
+    constructor(props) {
+        super(props)
+        this.deviceType = 'laptop'
+    }
 
-  render() {
-    const {device, update, options} = this.props
-    return (
-      <div>
-        <div className="row">
-          <div className="col-sm-6">
-            {this.inputField('mac', 'MAC address (wired)', false, 'XX:XX:XX:XX:XX:XX')}
-          </div>
-          <div className="col-sm-6">
-            {this.inputField('mac2', 'Secondary MAC address', false, 'XX:XX:XX:XX:XX:XX')}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6">
-            {this.inputField('manufacturer')}
-          </div>
-          <div className="col-sm-6">
-            {this.inputField('model')}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-4">
-            {this.inputField('processor', null, false, 'Intel core 5')}
-          </div>
-          <div className="col-sm-4">
-            {this.inputField('video_card', null, false, 'Nvidia, ATI, Onboard')}
-          </div>
-          <div className="col-sm-4">
-            {this.inputField('os', 'Operating system', false, 'Windows 10, Mac OS, Linux')}
-            <SelectFilter
-              value={device.os}
-              options={options.os}
-              update={update.bind(null, 'os')}
-              name="os"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-4">
-            {this.inputField('ram', 'RAM', false, 'in GB')}
-          </div>
-          <div className="col-sm-4">
-            {this.inputField('hd_size', 'Hard drive', false, 'GB or TB')}
-          </div>
-          <div className="col-sm-4">
-
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-4">
-            <div>
-              <BigCheckbox
-                checked={device.battery_backup === 1}
-                handle={update.bind(null, 'battery_backup', device.battery_backup === 1
-                ? 0
-                : 1)}
-                label="Battery backup"/>
-            </div>
-            <div>
-              <BigCheckbox
-                checked={device.redundant_backup === 1}
-                handle={update.bind(null, 'redundant_backup', device.redundant_backup === 1
-                ? 0
-                : 1)}
-                label="Redundant backup"/>
-            </div>
-            <div>
-              <BigCheckbox
-                checked={device.rotation === 1}
-                handle={update.bind(null, 'rotation', device.rotation === 1
-                ? 0
-                : 1)}
-                label="Exclude from rotation"/>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div>
-              <BigCheckbox
-                checked={device.touch_screen === 1}
-                handle={update.bind(null, 'touch_screen', device.touch_screen === 1
-                ? 0
-                : 1)}
-                label="Touch screen"/>
-            </div>
-            <div>
-              <BigCheckbox
-                checked={device.docking_station === 1}
-                handle={update.bind(null, 'docking_station', device.docking_station === 1
-                ? 0
-                : 1)}
-                label="Docking station"/>
-            </div>
-              <InputField
-                disabled={empty(device.docking_station)}
-                name="primary_monitor"
-                value={device.primary_monitor}
-                label="Primary Monitor Size"
-                change={update.bind(null, 'primary_monitor')}/>
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        const {device, update, options} = this.props
+        return (
+                <div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            {this.inputField('mac', 'MAC address (wired)', false, 'XX:XX:XX:XX:XX:XX')}
+                        </div>
+                        <div className="col-sm-6">
+                            {this.inputField('mac2', 'Secondary MAC address', false, 'XX:XX:XX:XX:XX:XX')}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            {this.inputField('manufacturer')}
+                        </div>
+                        <div className="col-sm-6">
+                            {this.inputField('model')}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-4">
+                            {this.inputField('processor', null, false, 'Intel core 5')}
+                        </div>
+                        <div className="col-sm-4">
+                            {this.inputField('video_card', null, false, 'Nvidia, ATI, Onboard')}
+                        </div>
+                        <div className="col-sm-4">
+                            {this.inputField('os', 'Operating system', false, 'Windows 10, Mac OS, Linux')}
+                            <SelectFilter
+                                value={device.os}
+                                options={options.os}
+                                update={update.bind(null, 'os')}
+                                name="os"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-4">
+                            {this.inputField('ram', 'RAM', false, 'in GB')}
+                        </div>
+                        <div className="col-sm-4">
+                            {this.inputField('hd_size', 'Hard drive', false, 'GB or TB')}
+                        </div>
+                        <div className="col-sm-4">
+                
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <div>
+                                <BigCheckbox
+                                    checked={device.battery_backup === 1}
+                                    handle={update.bind(null, 'battery_backup', device.battery_backup === 1
+                            ? 0
+                            : 1)}
+                                    label="Battery backup"/>
+                            </div>
+                            <div>
+                                <BigCheckbox
+                                    checked={device.redundant_backup === 1}
+                                    handle={update.bind(null, 'redundant_backup', device.redundant_backup === 1
+                            ? 0
+                            : 1)}
+                                    label="Redundant backup"/>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div>
+                                <BigCheckbox
+                                    checked={device.rotation === 1}
+                                    handle={update.bind(null, 'rotation', device.rotation === 1
+                            ? 0
+                            : 1)}
+                                    label="Exclude from rotation"/>
+                            </div>
+                            <div>
+                                <BigCheckbox
+                                    checked={device.touch_screen === 1}
+                                    handle={update.bind(null, 'touch_screen', device.touch_screen === 1
+                            ? 0
+                            : 1)}
+                                    label="Touch screen"/>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div>
+                                <BigCheckbox
+                                    checked={device.docking_station === 1}
+                                    handle={update.bind(null, 'docking_station', device.docking_station === 1
+                            ? 0
+                            : 1)}
+                                    label="Docking station"/>
+                            </div>
+                            <InputField
+                                disabled={empty(device.docking_station)}
+                                name="primary_monitor"
+                                value={device.primary_monitor}
+                                label="Primary Monitor Size"
+                                change={update.bind(null, 'primary_monitor')}/>
+                        </div>
+                    </div>
+                </div>
+                )
+    }
 }
