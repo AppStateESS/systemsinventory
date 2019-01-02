4e308e3296e1db5b9af9ceaa6537f4db639be353
Ted <eberhardtm@appstate.edu>
2019-01-02 08:42:21 -0500

    Add defines file

---
+++ config/defines.dist.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MIT License
+ * Copyright (c) 2017 Electronic Student Services @ Appalachian State University
+ * 
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ * 
+ * @author Ted Eberhard <eberhardtm@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+define("FACULTY_API_URL", "your staff banner api url");
+define("STUDENT_API_URL", "your student banner api url");
